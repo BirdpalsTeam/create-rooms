26419b1681e4dae16054d5267529f8e79161d3c6
Gasegamer <66225865+Gasegamer@users.noreply.github.com>
2021-11-26 21:58:46 -0300

    Add files via upload

--- create_room_pngs.js
+++ create_room_pngs.js
@@ -1,8 +1,9 @@
 var PSD = require('psd');
-let path = 'C:/users/...'; //The path to your psd, which is the same to save your images.
-let psdName = 'your.psd'; //The name of the psd
+let room = 'town';
+let path = `C:/Users/renat/Pictures/Birdpals/Create Room psd/examples/${room}`; //The path to your psd, which is the same to save your images.
+let psdName = `${room}.psd`; //The name of the psd
 var psd = PSD.fromFile(`${path}/${psdName}`);
-let prefix = 'your prefix';	//The prefix of the images you want to use
+let prefix = `${room}_`;	//The prefix of the images you want to use
 
 psd.parse();
 
