61c7da6a1d1cac0675fcccd117a44866836b222a
Gasegamer <66225865+Gasegamer@users.noreply.github.com>
2021-11-26 21:59:37 -0300

    Updated create room pngs script

---
+++ create_room_pngs.js
@@ -0,0 +1,20 @@
+var PSD = require('psd');
+let room = 'town';
+let path = `C:/Users/.../${room}`; //The path to your psd, which is the same to save your images.
+let psdName = `${room}.psd`; //The name of the psd
+var psd = PSD.fromFile(`${path}/${psdName}`);
+let prefix = `${room}_`;	//The prefix of the images you want to use
+
+psd.parse();
+
+psd.tree().descendants().forEach((node) => {
+	if(node.name == 'Images'){
+		node._children.forEach((layer) =>{
+			PSD.open(`${path}/${psdName}`).then(function (psd) {
+					return layer.layer.image.saveAsPng(`${path}/${prefix + layer.layer.name}.png`);
+				}).then(function () {
+				console.log(`Saved ${prefix + layer.layer.name}.png at ${path}`);
+			});
+		})
+	}
+});
